48eb82a35190614a5814ba94dabf01cf3147164f
yexnsxo <128563979+yexnsxo@users.noreply.github.com>
2025-03-30 22:03:26 +0900

    Feat: message.js 리셋 기능 추가

--- week03/message.js
+++ week03/message.js
@@ -3,6 +3,7 @@ function checkSubmit() {
   var Confirm = confirm (nickname + "님, 메세지를 보내시겠습니까?");
   
   if (!Confirm) {
+    document.getElementById("form").reset();
     return false;
   } else {
     alert("메세지를 전송하였습니다!");
