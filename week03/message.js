af5df1c16f3831d7a813662fd317917c41f0df09
yexnsxo <128563979+yexnsxo@users.noreply.github.com>
2025-03-30 20:44:41 +0900

    Feat: 파일 이름 변경

--- week03/message.js
+++ week03/message.js
@@ -1,10 +1,10 @@
 function checkSubmit() {
   const nickname = document.getElementById("Nickname").value;
-  var Confirm = confirm (nickname + "님, 질문을 제출하시겠습니까?");
+  var Confirm = confirm (nickname + "님, 메세지를 보내시겠습니까?");
   
   if (!Confirm) {
     e.preventDefault(); // 제출을 막습니다.
   } else {
-    alert("제출되었습니다!");
+    alert("메세지를 전송하였습니다!");
   }
   }
