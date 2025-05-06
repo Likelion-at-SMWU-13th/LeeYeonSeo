6197800bc686c8863afa69d9e5520939f47d1881
yexnsxo <128563979+yexnsxo@users.noreply.github.com>
2025-05-06 18:01:21 +0900

    fetch API 실습 완료

--- frontend/FEweek03/js/movieList.js
+++ frontend/FEweek03/js/movieList.js
@@ -13,5 +13,33 @@ const includeAc = movies.filter((movie)=> {
 console.log("액이 들어가는 장르의 영화 목록:");
 console.log(includeAc);
 
-const actionMovie = movies.filter((movie)=>movie.genre.includes("액"));
-console.log(actionMovie);
+// const actionMovie = movies.filter((movie)=>movie.genre.includes("액"));
+// console.log(actionMovie);
+
+async function fetchMovies() {
+  try {
+    const response = await fetch("movies.json"); //await 코드가 다 실행될때까지 뒤에 코드 기다림
+    const movies = await response.json();
+    const container = document.getElementById("movieContainer");
+    const movieList = movies.filter((movie) => movie.genre === "액션");
+
+    movieList.forEach((movie) => {
+      const card = document.createElement("div");
+      card.className = "card";
+
+      card.innerHTML = `
+      <img src="${movie.poster}" alt="${movie.title}">
+      <div class="card-body">
+        <h2 class="card-title">${movie.title}</h2>
+        <p class="card-rating">⭐️ ${movie.rating}</p>
+        <p class="card-description>${movie.description}</p>
+      </div>
+      `;
+      container.appendChild(card);
+    });
+  } catch (error) {
+    console.error("에러 발생:", error);
+  }
+}
+
+fetchMovies();
