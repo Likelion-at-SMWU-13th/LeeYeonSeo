91909365f4f85f4bbbb9ecd114ff53cbf63ec20f
yexnsxo <yslove829@naver.com>
2025-07-20 20:07:16 +0900

    [Comment] Redirection.jsx 주석 추가

--- frontend/FEweek08HW/week08hw/src/Redirection.jsx
+++ frontend/FEweek08HW/week08hw/src/Redirection.jsx
@@ -2,13 +2,15 @@ import React, { useEffect } from "react";
 import { useNavigate, useSearchParams } from "react-router-dom";
 
 const Redirection = () => {
-  const [params] = useSearchParams();
+  // 아까 받은 Authorization Code를 Redirect URI에서 추출
+  const [params] = useSearchParams(); // useSearchParams를 통해 현재 url의 쿼리 파라미터들을 params에 저장
+  const authCode = params.get("code"); // params에 저장된 쿼리 파라미터들 중 key 값이 "code"인 value 값을 authCode에 저장
 
-  const authCode = params.get("code");
-  const grant_type = "authorization_code";
+  const grant_type = "authorization_code"; // 카카오에 요청을 보낼 때 필요한 쿼리 파라미터 중 하나로 값은 authorization_code로 고정해야 함
   const navigate = useNavigate();
 
   useEffect(() => {
+    // 사전에 설정한 REST API 키와 Redirect URI, Authorization Code 값을 쿼리 파라미터로 싣고 Access Token을 요청
     fetch(
       `https://kauth.kakao.com/oauth/token?grant_type=${grant_type}&client_id=${
         import.meta.env.VITE_REST_API_KEY
@@ -21,10 +23,10 @@ const Redirection = () => {
         },
       }
     ).then((res) => {
-      const data = res.json();
+      const data = res.json(); // 받아온 응답을 json 객체로 만들어줌
       data.then((data) => {
-        localStorage.setItem("accessToken", data.access_token);
-        navigate("/greeting");
+        localStorage.setItem("accessToken", data.access_token); // 요청에 대한 응답으로 넘어온 액세스 토큰을 브라우저의 로컬 스토리지에 저장
+        navigate("/greeting"); // greeting 페이지로 이동
       });
     });
   }, [authCode, grant_type, navigate]);
