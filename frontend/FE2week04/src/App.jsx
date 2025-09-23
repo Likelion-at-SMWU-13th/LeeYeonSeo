0dd2c034efc09afea98bb1075e6f6e0a55fb9f2f
yexnsxo <yslove829@naver.com>
2025-09-23 20:06:21 +0900

    [Feat] useQuery를 사용한 데이터 조회 완료

--- frontend/FE2week04/src/App.jsx
+++ frontend/FE2week04/src/App.jsx
@@ -1,7 +1,28 @@
+import { useQuery } from "@tanstack/react-query";
 import "./App.css";
 
 function App() {
-  return <>tanstack query 실습!</>;
+  async function getPosts() {
+    const reponse = await fetch(`https://jsonplaceholder.typicode.com/posts`);
+    return await reponse.json();
+  }
+  const { data: postsData } = useQuery({
+    queryKey: ["posts"],
+    queryFn: getPosts,
+  });
+  console.log("postsResult", postsData);
+
+  const posts = postsData ?? [];
+
+  return (
+    <>
+      <ul>
+        {posts.map((post) => (
+          <li key={post.id}>{post.title}</li>
+        ))}
+      </ul>
+    </>
+  );
 }
 
 export default App;
