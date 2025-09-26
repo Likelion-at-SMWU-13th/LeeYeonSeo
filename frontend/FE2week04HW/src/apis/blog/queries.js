7d3f7d23ae37670eb332962f0363dceb53e1b9ae
yexnsxo <yslove829@naver.com>
2025-09-26 22:33:51 +0900

    [Feat] 마이페이지 조회 기능 구현

--- frontend/FE2week04HW/src/apis/blog/queries.js
+++ frontend/FE2week04HW/src/apis/blog/queries.js
@@ -4,6 +4,19 @@ import { createUser, readUser, updateUser, deleteUser } from "./axios";
 export const useCreateUser = () => {
   return useMutation({
     mutationFn: ({ username, password }) => createUser({ username, password }),
-    onSuccess: () => alert("환영합니다"),
+    onSuccess: () => alert("환영합니다"), // 회원가입 성공 시 "환영합니다" 알림
+  });
+};
+
+export const useReasUser = (userId) => {
+  return useQuery({
+    queryKey: ["users", userId],
+    queryFn: () => {
+      readUser(userId);
+    },
+    enabled: !!userId,
+    staleTime: 30 * 1000, // 데이터를 30초 동안 신선한 상태로 유지
+    refetchInterval: 30 * 1000, // 데이터가 stale 상태가 되었을 때 재요청
+    retry: 3, // 실패 시 최대 3번까지 재시도
   });
 };
