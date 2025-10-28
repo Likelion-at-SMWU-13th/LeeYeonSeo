5bbff56fb0c95589e4c1e9d58016871af32c4d65
yexnsxo <yslove829@naver.com>
2025-10-28 19:34:30 +0900

    [Feat] Comment.tsx 타입 선언 완료

--- frontend/FE2week06/src/constants/comment.ts
+++ frontend/FE2week06/src/constants/comment.ts
@@ -1,3 +1,11 @@
+// 한개의 댓글 객체에 대한 타입
+export interface CommentItemType {
+  id: number;
+  author: string;
+  comment: string;
+  timestamp: string;
+}
+
 export const COMMENT_DATA = [
   {
     id: 1,
